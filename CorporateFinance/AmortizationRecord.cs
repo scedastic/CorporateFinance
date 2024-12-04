a6afa932997d97b4f0c4b5d0775a3937f06c3666
Yosef Sheinfil <scedastic@users.noreply.github.com>
2024-12-04 16:12:18 -0500

    Add files via upload

---
+++ CorporateFinance/AmortizationRecord.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateFinance
+{
+    /// <summary>
+    /// Represents a single row in an Amortization Table.
+    /// </summary>
+    public class AmortizationRecord
+    {
+        /// <summary>
+        /// Which payment number does this record refer to.
+        /// </summary>
+        public int SequenceID { get; set; }
+        /// <summary>
+        /// The amount of Principal left at the beginning of this period.
+        /// </summary>
+        public double Principal {  get; set; }
+        /// <summary>
+        /// The amount of interest at the end of this period.
+        /// </summary>
+        public double Interest {  get; set; }
+        /// <summary>
+        /// The payment amount that is expected at the end of this period.
+        /// </summary>
+        public double Payment { get; set; }
+
+        /// <summary>
+        /// The portion of the payment that is applied to the remaining principal.
+        /// </summary>
+        public double PaymentAppliedToPrincipal
+        {
+            get
+            {
+                return Payment - Interest;
+            }
+        }
+
+    }
+}
---
+++ CorporateFinance/AmortizationSchedule.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateFinance
+{
+    public class AmortizationSchedule
+    {
+        private double _principal;
+        private double _apr;
+        private int _years;
+        private double _monthlyPayment;
+        public double Principal 
+        { 
+            get { return _principal; } 
+            set
+            {
+                _principal = value;
+                ResetSchedule();
+            }
+        }
+
+        public double APR 
+        {  
+            get { return _apr; }
+            set
+            {
+                _apr = value;
+                ResetSchedule();
+            }
+        }
+
+        public int Years
+        {
+            get { return _years; }
+            set
+            {
+                _years = value;
+                ResetSchedule();
+            }
+        }
+
+        public double MonthlyPayment { get { return _monthlyPayment; } }
+
+        public List<AmortizationRecord> AmortizationTable { get; set; }
+        public AmortizationSchedule(double principal, double apr, int years) 
+        { 
+            _principal = principal;
+            _apr = apr;
+            _years = years;
+            _monthlyPayment = Valuations.GetMortgagePayment(principal, apr, years);
+            ResetSchedule();
+        }
+        private void ResetSchedule()
+        {
+            double principalLeft = _principal;
+            AmortizationTable = new List<AmortizationRecord>();
+            for(int i = 0; i < _years * 12; i++)
+            {
+                var amortization = new AmortizationRecord();
+                amortization.SequenceID = i + 1;
+                amortization.Payment = _monthlyPayment;
+                amortization.Principal = principalLeft;
+                amortization.Interest = principalLeft * _apr / 12;
+                AmortizationTable.Add(amortization);
+                principalLeft = amortization.Principal + amortization.Interest - MonthlyPayment;
+            }
+        }
+    }
+}
